42af4f84abfb3b66d7fca2dd685cebd3fdfb5829
hkanezashi <kanezashi.h@gmail.com>
2019-11-28 17:47:22 +0900

    Redirect console outputs to file in run_batch.sh

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -6,12 +6,15 @@ if [[ $# -lt 1 ]]; then
 fi
 
 CONF_JSON=$1
-OUTPUT_LOG=${2:-/dev/stdout}
+OUTPUT_LOG=${2:-/dev/null}
 
 echo "Configuration JSON file: ${CONF_JSON}"
 echo "Output Log file: ${OUTPUT_LOG}"
 
-python3 scripts/transaction_graph_generator.py "${CONF_JSON}" > "${OUTPUT_LOG}" 2>&1
-sh scripts/run_AMLSim.sh "${CONF_JSON}" >> "${OUTPUT_LOG}" 2>&1
-python3 scripts/convert_logs.py "${CONF_JSON}" >> "${OUTPUT_LOG}" 2>&1
+python3 scripts/transaction_graph_generator.py "${CONF_JSON}" 2>&1 | tee "${OUTPUT_LOG}"
+sh scripts/run_AMLSim.sh "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
+python3 scripts/convert_logs.py "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
+
+#python3 scripts/validation/validate_alerts.py "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
+#python3 scripts/visualize/plot_distributions.py "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
 

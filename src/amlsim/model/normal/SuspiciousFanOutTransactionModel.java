f0c1262f1184121a686948487b20af7a7188ca18
hkanezashi <kanezashi.h@gmail.com>
2019-12-03 23:18:20 +0900

    Adjust transaction amount and frequency of SAR accounts in the simulator

--- src/amlsim/model/normal/SuspiciousFanOutTransactionModel.java
+++ src/amlsim/model/normal/SuspiciousFanOutTransactionModel.java
@@ -26,21 +26,23 @@ public class SuspiciousFanOutTransactionModel extends AbstractTransactionModel {
 
     @Override
     public void sendTransaction(long step) {
-        List<Account> beneList = this.account.getBeneList();
-        int numBene = beneList.size();
-
-        int index = (int)(step - startStep) % interval;
-        if(index < 0){
-            index += interval;
-        }
-//        System.out.println(numBene);
-        if(index >= numBene || numBene == 0){  // No more destination accounts
+        if((step - startStep) % interval != 0){
             return;
         }
+        List<Account> beneList = this.account.getBeneList();
+//        int numBene = beneList.size();
+//        int index = (int)(step - startStep) % interval;
+//        if(index < 0){
+//            index += interval;
+//        }
+//        if(index >= numBene || numBene == 0){  // No more destination accounts
+//            return;
+//        }
 
-        float amount = getSuspiciousTransactionAmount();
-//        System.out.println("Suspicious fan-out: " + step + " " + amount);
-        Account dest = beneList.get(index);
-        this.sendTransaction(step, amount, dest);
+        for(Account dest : beneList) {
+//                    Account dest = beneList.get(index);
+            float amount = getSuspiciousTransactionAmount();
+            this.sendTransaction(step, amount, dest);
+        }
     }
 }

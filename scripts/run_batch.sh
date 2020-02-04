1cb3a0ab533f7df1213c495c404f27f781a3a8d1
hkanezashi <kanezashi.h@gmail.com>
2020-02-04 17:38:11 +0900

    Add missing configuration parameters #30 #31 and remove unnecessary temporal files

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -7,11 +7,12 @@ fi
 
 CONF_JSON=$1
 OUTPUT_LOG=${2:-/dev/null}
+EDGE_RATIO=${3:-0.0}
 
 echo "Configuration JSON file: ${CONF_JSON}"
 echo "Output Log file: ${OUTPUT_LOG}"
 
-time python3 scripts/transaction_graph_generator.py "${CONF_JSON}" 2>&1 | tee "${OUTPUT_LOG}"
+time python3 scripts/transaction_graph_generator.py "${CONF_JSON}" "${EDGE_RATIO}" 2>&1 | tee "${OUTPUT_LOG}"
 time sh scripts/run_AMLSim.sh "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
 time python3 scripts/convert_logs.py "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
 

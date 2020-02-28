3eb11b894b1c22cfa5c5cf2a28ce46cd04104958
hkanezashi <kanezashi.h@gmail.com>
2020-02-28 12:17:54 +0900

    Exit batch script if failed one of the process fails

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -12,9 +12,22 @@ EDGE_RATIO=$3
 echo "Configuration JSON file: ${CONF_JSON}"
 echo "Output Log file: ${OUTPUT_LOG}"
 
-time python3 scripts/transaction_graph_generator.py "${CONF_JSON}" "${EDGE_RATIO}" 2>&1 | tee "${OUTPUT_LOG}"
-time sh scripts/run_AMLSim.sh "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
-time python3 scripts/convert_logs.py "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
+SHELL_PROC=$$
+
+function failed() {
+    echo "Failed: $1" >&2
+    kill $SHELL_PROC  # Exit this script
+}
+
+function run(){
+  (time $1 || failed "$1") 2>&1 | tee -a "$OUTPUT_LOG"
+}
+
+run "python3 scripts/transaction_graph_generator.py ${CONF_JSON} ${EDGE_RATIO}"
+
+run "sh scripts/run_AMLSim.sh ${CONF_JSON}"
+
+run "python3 scripts/convert_logs.py ${CONF_JSON}"
 
 #python3 scripts/validation/validate_alerts.py "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"
 #python3 scripts/visualize/plot_distributions.py "${CONF_JSON}" 2>&1 | tee -a "${OUTPUT_LOG}"

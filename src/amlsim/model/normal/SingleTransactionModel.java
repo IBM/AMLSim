ed410ece9c8abcf4311c6b2bd79c3f3e0227a17c
hkanezashi <kanezashi.h@gmail.com>
2020-02-28 13:48:29 +0900

    Fix SingleTransactionModel to make a transaction for each account #34

--- src/amlsim/model/normal/SingleTransactionModel.java
+++ src/amlsim/model/normal/SingleTransactionModel.java
@@ -1,5 +1,6 @@
 package amlsim.model.normal;
 
+import amlsim.AMLSim;
 import amlsim.Account;
 import amlsim.model.AbstractTransactionModel;
 
@@ -7,58 +8,72 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * Send money only for once to each neighbor account
+ * Send money only for once to one of the neighboring accounts
  */
 public class SingleTransactionModel extends AbstractTransactionModel {
 
-    private int index = 0;
+//    private int index = 0;
     private static Random rand = new Random();
-    private long[] steps;
-
+//    private long[] steps;
+    private long txStep = -1;
+    
     public String getType(){
         return "Single";
     }
 
     public void setParameters(int interval, float balance, long start, long end){
         super.setParameters(interval, balance, start, end);
-        if(this.startStep < 0){  // decentralize the first transaction step
-            this.startStep = generateStartStep(interval);
+//        if(this.startStep < 0){  // decentralize the first transaction step
+//            this.startStep = generateStartStep((int) AMLSim.getNumOfSteps());
+//        }
+        if(this.startStep < 0){
+            this.startStep = 0;
         }
+        if(this.endStep < 0){
+            this.endStep = AMLSim.getNumOfSteps();
+        }
+        this.txStep = this.startStep + rand.nextInt((int)(endStep - startStep + 1));
     }
-
+    
     public void sendTransaction(long step){
         List<Account> beneList = this.account.getBeneList();
         int numBene = beneList.size();
-        
-        if(step < this.startStep || this.endStep < step || numBene == 0){
+        if(step != this.txStep || numBene == 0){
             return;
         }
-        if(step == this.startStep){
-            steps = rand.longs(numBene, this.startStep, this.endStep + 1).sorted().toArray();
-        }
-
-        if(index >= numBene){
-            index = 0;
-        }
+        
+//        if(step < this.startStep || this.endStep < step || numBene == 0){
+//            return;
+//        }
+//        if(step == this.startStep){
+//            steps = rand.longs(numBene, this.startStep, this.endStep + 1).sorted().toArray();
+//        }
+//
+//        if(index >= numBene){
+//            index = 0;
+//        }
 
         float amount = getTransactionAmount();
-        long stepRange = this.endStep - this.startStep + 1;
-        int numPerStep = numBene / (int)stepRange;
-
-        if(numPerStep == 0){
-            if(step == steps[index]) {
-                Account dest = beneList.get(index);
-                this.sendTransaction(step, amount, dest);
-                index++;
-            }
-        }else{
-            int start = index;
-            int end = Math.min(index + numPerStep, numBene);
-            for(int i=start; i<end; i++){
-                Account dest = beneList.get(i % numBene);
-                this.sendTransaction(step, amount, dest);
-            }
-            index += numPerStep;
-        }
+        int index = rand.nextInt(numBene);
+        Account dest = beneList.get(index);
+        this.sendTransaction(step, amount, dest);
+//        long stepRange = this.endStep - this.startStep + 1;
+//        int numPerStep = numBene / (int)stepRange;
+//
+//        if(numPerStep == 0){
+//            if(step == steps[index]) {
+//                Account dest = beneList.get(index);
+//                this.sendTransaction(step, amount, dest);
+//                index++;
+//            }
+//        }else{
+//            int start = index;
+//            int end = Math.min(index + numPerStep, numBene);
+//            for(int i=start; i<end; i++){
+//                Account dest = beneList.get(i % numBene);
+//                this.sendTransaction(step, amount, dest);
+//            }
+//            index += numPerStep;
+//        }
     }
 }

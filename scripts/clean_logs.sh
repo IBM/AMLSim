fb7eafc1e73c6b2aeb25189ddf33235ad0a670d7
Jordan D. Nelson <Jordan.doug.nelson@gmail.com>
2021-10-28 11:59:46 -0500

    53: fix clean_logs.sh script

--- scripts/clean_logs.sh
+++ scripts/clean_logs.sh
@@ -8,6 +8,8 @@ rm -f ${OUTPUT}/*.csv
 rm -f ${OUTPUT}/*.png
 rm -f ${OUTPUT}/*.gif
 
+find ${OUTPUT} -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \;
+
 if [ -d ${TMP} ]; then
 rm -rf ${TMP:?}/*/
 fi
